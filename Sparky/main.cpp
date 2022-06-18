e3a05f58c64baa078abcd1b42a48271fa4b95347
brccabral <cabral.brc@gmail.com>
2022-06-17 19:25:38 -0700

    print GL version

--- Sparky/main.cpp
+++ Sparky/main.cpp
@@ -11,6 +11,8 @@ int main()
     Window window("Sparky!", 800, 600);
     glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
 
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
     while (!window.closed())
     {
         window.clear();
