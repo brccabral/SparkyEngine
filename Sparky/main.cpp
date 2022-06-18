34f1fef4bddfdca72091f004a56ec0f626949963
brccabral <cabral.brc@gmail.com>
2022-06-17 19:29:57 -0700

    draw a rectangle

--- Sparky/main.cpp
+++ Sparky/main.cpp
@@ -16,9 +16,10 @@ int main()
     while (!window.closed())
     {
         window.clear();
-        glBegin(GL_TRIANGLES);
+        glBegin(GL_QUADS);
         glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
+        glVertex2f(-0.5f, 0.5f);
+        glVertex2f(0.5f, 0.5f);
         glVertex2f(0.5f, -0.5f);
         glEnd();
         window.update();
