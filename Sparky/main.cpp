3a519ebeee161dc17d5dcee82e40ff8c2415f84f
brccabral <cabral.brc@gmail.com>
2022-06-20 01:17:58 -0700

    draw a pink square made of two triangles

--- Sparky/main.cpp
+++ Sparky/main.cpp
@@ -16,15 +16,32 @@ int main()
     Window window("Sparky!", 960, 540);
     glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
 
-    GLuint vao;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
+    // draw two triangles to form a rectangle
+    GLfloat vertices[] = {
+        // draw one triangle on top left
+        -0.5f, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f,
+        0.5f, 0.5f, 0.0f,
+        // draw one triangle on bottom right
+        0.5f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f };
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0); // group vertices at each 3 verts
+    glEnableVertexAttribArray(0);
 
     Shader shader("src/shaders/basic.vert", "src/shaders/basic.frag");
+    shader.enable();
 
     while (!window.closed())
     {
         window.clear();
+
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         window.update();
     }
