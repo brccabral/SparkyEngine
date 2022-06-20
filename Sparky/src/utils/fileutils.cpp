78d00030d182b6e131cb1ab02d9fa5fe52d558ab
brccabral <cabral.brc@gmail.com>
2022-06-20 01:24:20 -0700

    shorter read_file

--- Sparky/src/utils/fileutils.cpp
+++ Sparky/src/utils/fileutils.cpp
@@ -6,9 +6,8 @@ namespace sparky
 {
     std::string read_file(const std::string& filepath)
     {
-        std::ifstream ifs((filepath).c_str());
         std::string content(
-            std::istreambuf_iterator<char>(ifs.rdbuf()),
+            std::istreambuf_iterator<char>(std::ifstream(filepath).rdbuf()),
             std::istreambuf_iterator<char>());
         return content;
     }
