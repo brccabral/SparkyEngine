753f5e159ee21fc13eb8454ce7293bb639bfbfd8
brccabral <cabral.brc@gmail.com>
2022-06-20 01:52:13 -0700

    draw rectangle using buffers

--- Sparky/main.cpp
+++ Sparky/main.cpp
@@ -7,6 +7,10 @@
 #include "src/utils/fileutils.h"
 #include "src/graphics/shader.h"
 
+#include "src/graphics/buffers/buffer.h"
+#include "src/graphics/buffers/indexbuffer.h"
+#include "src/graphics/buffers/vertexarray.h"
+
 int main()
 {
     using namespace sparky;
@@ -14,25 +18,22 @@ int main()
     using namespace maths;
 
     Window window("Sparky!", 960, 540);
-    // glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
 
     // draw two triangles to form a rectangle
     GLfloat vertices[] = {
-        // draw one triangle on top left
         0, 0, 0,
-        8, 0, 0,
-        0, 3, 0,
-        // draw one triangle on bottom right
         0, 3, 0,
         8, 3, 0,
         8, 0, 0 };
+    // indices of vertices in order to create two triangles
+    // that will form a rectangle
+    GLushort indices[] = { 0, 1, 2, 2, 3, 0 };
 
-    GLuint vbo;
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0); // group vertices at each 3 verts
-    glEnableVertexAttribArray(0);
+    VertexArray vao;
+    Buffer* vbo = new Buffer(vertices, 4 * 3, 3);
+    IndexBuffer ibo(indices, 6);
+
+    vao.addBuffer(vbo, 0);
 
     Shader shader("src/shaders/basic.vert", "src/shaders/basic.frag");
     shader.enable();
@@ -52,7 +53,11 @@ int main()
         window.getMousePosition(x, y);
         shader.setUniform("light_pos", vec2((float)(x * 16.0f / 960.0f - 4.0f), (float)(9.0f - y * 9.0f / 540.0f - 3.0f)));
 
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        vao.bind();
+        ibo.bind();
+        glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_SHORT, 0);
+        ibo.unbind();
+        vao.unbind();
 
         window.update();
     }
