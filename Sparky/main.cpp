63c29bd47973660c6d4470a5104f164bba0e5729
brccabral <cabral.brc@gmail.com>
2022-06-20 01:59:30 -0700

    show two Renderable2D on screen

--- Sparky/main.cpp
+++ Sparky/main.cpp
@@ -22,37 +22,6 @@ int main()
 
     Window window("Sparky!", 960, 540);
 
-    // draw two triangles to form a rectangle
-    GLfloat vertices[] = {
-        0, 0, 0,
-        0, 3, 0,
-        8, 3, 0,
-        8, 0, 0 };
-    // indices of vertices in order to create two triangles
-    // that will form a rectangle
-    GLushort indices[] = { 0, 1, 2, 2, 3, 0 };
-
-    GLfloat colorsA[] = {
-        1, 0, 1, 1,
-        1, 0, 1, 1,
-        1, 0, 1, 1,
-        1, 0, 1, 1 };
-
-    GLfloat colorsB[] = {
-        0.2f, 0.3f, 0.8f, 1,
-        0.2f, 0.3f, 0.8f, 1,
-        0.2f, 0.3f, 0.8f, 1,
-        0.2f, 0.3f, 0.8f, 1 };
-
-    VertexArray sprite1, sprite2;
-    IndexBuffer ibo(indices, 6);
-
-    sprite1.addBuffer(new Buffer(vertices, 4 * 3, 3), 0);
-    sprite1.addBuffer(new Buffer(colorsA, 4 * 4, 4), 1);
-
-    sprite2.addBuffer(new Buffer(vertices, 4 * 3, 3), 0);
-    sprite2.addBuffer(new Buffer(colorsB, 4 * 4, 4), 1);
-
     mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
 
     Shader shader("src/shaders/basic.vert", "src/shaders/basic.frag");
@@ -61,6 +30,7 @@ int main()
     shader.setUniform("ml_matrix", mat4::translation(vec3(4, 3, 0)));
 
     Renderable2D sprite(vec3(5, 5, 0), vec2(4, 4), vec4(1, 0, 1, 1), shader);
+    Renderable2D sprite2(vec3(7, 1, 0), vec2(2, 3), vec4(0.2f, 0, 1, 1), shader);
     Simple2Drenderer renderer;
 
     shader.setUniform("light_pos", vec2(4.0f, 1.5f));
@@ -75,6 +45,7 @@ int main()
         shader.setUniform("light_pos", vec2((float)(x * 16.0f / 960.0f), (float)(9.0f - y * 9.0f / 540.0f)));
 
         renderer.submit(&sprite);
+        renderer.submit(&sprite2);
         renderer.flush();
 
         window.update();
