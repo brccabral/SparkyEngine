5ba090a77d7e0f73078eea565da2fa7155fc9ee5
brccabral <cabral.brc@gmail.com>
2022-06-20 01:24:03 -0700

    faster read_file

--- Sparky/src/utils/fileutils.cpp
+++ Sparky/src/utils/fileutils.cpp
@@ -1,26 +1,16 @@
 #include "fileutils.h"
 #include <string>
-#include <string.h>
+#include <fstream>
 
 namespace sparky
 {
-    std::string read_file(const char* filepath)
+    std::string read_file(const std::string& filepath)
     {
-        FILE* file = fopen(filepath, "rt"); // rt = read as text (line ending is like text file)
-        fseek(file, 0, SEEK_END);
-
-        unsigned long length = ftell(file);  // get number of bytes
-        char* data = new char[length + 1](); // allocate in HEAP, () puts 0 in all array
-
-        fseek(file, 0, SEEK_SET);     // go back to beginning
-        fread(data, 1, length, file); // read file and put in data, 1 byte = sizeof(char)
-        fclose(file);
-
-        std::string result(data);
-
-        // delete from HEAP
-        delete[] data;
-
-        return result;
+        std::ifstream ifs((filepath).c_str());
+        std::string content(
+            std::istreambuf_iterator<char>(ifs.rdbuf()),
+            std::istreambuf_iterator<char>());
+        return content;
     }
+
 } // namespace sparky
--- Sparky/src/utils/fileutils.h
+++ Sparky/src/utils/fileutils.h
@@ -4,6 +4,6 @@
 
 namespace sparky
 {
-    std::string read_file(const char* filepath);
+    std::string read_file(const std::string& filepath);
 
 } // namespace sparky
