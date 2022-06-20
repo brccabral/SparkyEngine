915e5700ce326edadcb412248c0f301b78d9e7d8
brccabral <cabral.brc@gmail.com>
2022-06-20 02:12:03 -0700

    generate many sprites

--- Sparky/main.cpp
+++ Sparky/main.cpp
@@ -1,6 +1,8 @@
 #include <GL/glew.h> // need to include before GLFW
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <vector>
+#include <time.h>
 
 #include "src/graphics/window.h"
 #include "src/maths/maths.h"
@@ -25,8 +27,16 @@ int main()
     shader.enable();
     shader.setUniform("pr_matrix", ortho);
 
-    Sprite sprite(5, 5, 4, 4, vec4(1, 0, 1, 1));
-    Sprite sprite2(7, 1, 2, 3, vec4(0.2f, 0, 1, 1));
+    srand(time(NULL));
+    std::vector<Renderable2D*> sprites;
+    for (float y = 0; y < 9.0f; y++)
+    {
+        for (float x = 0; x < 16.0f; x++)
+        {
+            sprites.push_back(new Sprite(x, y, 0.9f, 0.9f, vec4(rand() % 1000 / 1000.0f, 0, 1, 1)));
+        }
+    }
+
     BatchRederer2D renderer;
 
     shader.setUniform("light_pos", vec2(4.0f, 1.5f));
@@ -36,12 +46,14 @@ int main()
 
     while (!window.closed())
     {
-        renderer.begin();
         window.clear();
         window.getMousePosition(x, y);
         shader.setUniform("light_pos", vec2((float)(x * 16.0f / 960.0f), (float)(9.0f - y * 9.0f / 540.0f)));
-        renderer.submit(&sprite);
-        renderer.submit(&sprite2);
+        renderer.begin();
+        for (int i = 0; i < sprites.size(); i++)
+        {
+            renderer.submit(sprites[i]);
+        }
         renderer.end();
         renderer.flush();
 
