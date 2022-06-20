b621482bb592c067a83d556e2db5a9a9d5ea7c5d
brccabral <cabral.brc@gmail.com>
2022-06-20 01:55:33 -0700

    set colors to rectangles

--- Sparky/main.cpp
+++ Sparky/main.cpp
@@ -29,11 +29,26 @@ int main()
     // that will form a rectangle
     GLushort indices[] = { 0, 1, 2, 2, 3, 0 };
 
-    VertexArray vao;
-    Buffer* vbo = new Buffer(vertices, 4 * 3, 3);
+    GLfloat colorsA[] = {
+        1, 0, 1, 1,
+        1, 0, 1, 1,
+        1, 0, 1, 1,
+        1, 0, 1, 1 };
+
+    GLfloat colorsB[] = {
+        0.2f, 0.3f, 0.8f, 1,
+        0.2f, 0.3f, 0.8f, 1,
+        0.2f, 0.3f, 0.8f, 1,
+        0.2f, 0.3f, 0.8f, 1 };
+
+    VertexArray sprite1, sprite2;
     IndexBuffer ibo(indices, 6);
 
-    vao.addBuffer(vbo, 0);
+    sprite1.addBuffer(new Buffer(vertices, 4 * 3, 3), 0);
+    sprite1.addBuffer(new Buffer(colorsA, 4 * 4, 4), 1);
+
+    sprite2.addBuffer(new Buffer(vertices, 4 * 3, 3), 0);
+    sprite2.addBuffer(new Buffer(colorsB, 4 * 4, 4), 1);
 
     mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
 
@@ -53,14 +68,19 @@ int main()
         window.getMousePosition(x, y);
         shader.setUniform("light_pos", vec2((float)(x * 16.0f / 960.0f), (float)(9.0f - y * 9.0f / 540.0f)));
 
-        vao.bind();
+        sprite1.bind();
         ibo.bind();
         shader.setUniform("ml_matrix", mat4::translation(vec3(4, 3, 0)));
         glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_SHORT, 0);
+        ibo.unbind();
+        sprite1.unbind();
+
+        sprite2.bind();
+        ibo.bind();
         shader.setUniform("ml_matrix", mat4::translation(vec3(0, 0, 0)));
         glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_SHORT, 0);
         ibo.unbind();
-        vao.unbind();
+        sprite2.unbind();
 
         window.update();
     }
