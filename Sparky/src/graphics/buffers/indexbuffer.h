8cf368d60acf6094e0fd3b6dfe742f1df4fac20b
brccabral <cabral.brc@gmail.com>
2022-06-20 01:26:38 -0700

    IndexBuffer init

---
+++ Sparky/src/graphics/buffers/indexbuffer.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <GL/glew.h>
+
+namespace sparky::graphics
+{
+    class IndexBuffer
+    {
+    private:
+        GLuint m_BufferID;
+        GLsizei m_Count;
+
+    public:
+        IndexBuffer(GLushort* data, GLsizei count);
+
+        void bind() const;
+        void unbind() const;
+
+        inline GLsizei getCount() const { return m_Count; };
+    };
+
+} // namespace sparky::graphics
