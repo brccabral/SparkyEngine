58b5680f9bf58c352a0563e3ec4d69981414ba6b
brccabral <cabral.brc@gmail.com>
2022-06-20 02:00:50 -0700

    deconstructors to delete GL buffers

--- Sparky/src/graphics/buffers/indexbuffer.cpp
+++ Sparky/src/graphics/buffers/indexbuffer.cpp
@@ -10,6 +10,10 @@ namespace sparky::graphics
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(GLushort), data, GL_STATIC_DRAW);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     }
+    IndexBuffer::~IndexBuffer()
+    {
+        glDeleteBuffers(1, &m_BufferID);
+    };
 
     void IndexBuffer::bind() const
     {
