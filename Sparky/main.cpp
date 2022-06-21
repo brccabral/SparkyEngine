213cb1e40bfe1e80aaa37e78828f6dd8a4bd5c57
brccabral <cabral.brc@gmail.com>
2022-06-20 18:18:44 -0700

    include FreeImage

--- Sparky/main.cpp
+++ Sparky/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <time.h>
+#include <FreeImage.h>
 
 #include "src/graphics/window.h"
 #include "src/maths/maths.h"
@@ -13,6 +14,7 @@
 
 #define TEST_50K_SPRITE 0
 
+#if 0
 int main()
 {
     using namespace sparky;
@@ -83,6 +85,68 @@ int main()
             frames = 0;
         }
     }
+
+    return 0;
+}
+#endif
+
+int main() {
+    const char* filename = "test.png";
+
+    //image format
+    FREE_IMAGE_FORMAT fif = FIF_UNKNOWN;
+    //pointer to the image, once loaded
+    FIBITMAP* dib(0);
+    //pointer to the image data
+    BYTE* bits(0);
+    //image width and height
+    unsigned int width(0), height(0);
+    //OpenGL's image ID to map to
+    GLuint gl_texID;
+
+    //check the file signature and deduce its format
+    fif = FreeImage_GetFileType(filename, 0);
+    //if still unknown, try to guess the file format from the file extension
+    if (fif == FIF_UNKNOWN)
+        fif = FreeImage_GetFIFFromFilename(filename);
+    //if still unkown, return failure
+    if (fif == FIF_UNKNOWN)
+        return false;
+
+    //check that the plugin has reading capabilities and load the file
+    if (FreeImage_FIFSupportsReading(fif))
+        dib = FreeImage_Load(fif, filename);
+    //if the image failed to load, return failure
+    if (!dib)
+        return false;
+
+    //retrieve the image data
+    bits = FreeImage_GetBits(dib);
+    //get the image width and height
+    width = FreeImage_GetWidth(dib);
+    height = FreeImage_GetHeight(dib);
+    //if this somehow one of these failed (they shouldn't), return failure
+    if ((bits == 0) || (width == 0) || (height == 0))
+        return false;
+
+    std::cout << width << ", " << height << std::endl;
+
+    ////if this texture ID is in use, unload the current texture
+    //if (m_texID.find(texID) != m_texID.end())
+    //    glDeleteTextures(1, &(m_texID[texID]));
+
+    ////generate an OpenGL texture ID for this texture
+    //glGenTextures(1, &gl_texID);
+    ////store the texture ID mapping
+    //m_texID[texID] = gl_texID;
+    ////bind to the new texture ID
+    //glBindTexture(GL_TEXTURE_2D, gl_texID);
+    ////store the texture data for OpenGL use
+    //glTexImage2D(GL_TEXTURE_2D, level, internal_format, width, height,
+    //    border, image_format, GL_UNSIGNED_BYTE, bits);
+
+    ////Free FreeImage's copy of the data
+    //FreeImage_Unload(dib);
 
     return 0;
 }
