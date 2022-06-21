1ab8dfb0efd1da3d077f968394fd66e21c34ef00
brccabral <cabral.brc@gmail.com>
2022-06-21 01:57:05 -0700

    revert shaders to show many sprites

--- Sparky/main.cpp
+++ Sparky/main.cpp
@@ -1,6 +1,6 @@
 // #include <iostream>
 // #include <vector>
- #include <GL/glew.h>
+#include <GL/glew.h>
 
 #if 1
 #include "src/graphics/window.h"
@@ -17,8 +17,6 @@
 #include "src/graphics/layers/group.h"
 #include "src/graphics/texture.h"
 
-#define TEST_50K_SPRITE 0
-
 int main()
 {
     using namespace sparky;
@@ -31,17 +29,12 @@ int main()
     mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
 
     Shader *s = new Shader("src/shaders/basic.vert", "src/shaders/basic.frag");
-    Shader *s2 = new Shader("src/shaders/basic.vert", "src/shaders/basic.frag");
     Shader &shader = *s;
-    Shader &shader2 = *s2;
     shader.enable();
-    // shader2.enable();
     shader.setUniform("light_pos", vec2(4.0f, 1.5f));
-    // shader2.setUniform("light_pos", vec2(4.0f, 1.5f));
 
     TileLayer layer(&shader);
 
-#if TEST_50K_SPRITE
     for (float y = -9.0f; y < 9.0f; y += 0.1)
     {
         for (float x = -16.0f; x < 16.0f; x += 0.1)
@@ -49,22 +42,7 @@ int main()
             layer.add(new Sprite(x, y, 0.09f, 0.09f, vec4(rand() % 1000 / 1000.0f, 0, 1, 1)));
         }
     }
-#else
-    // mat4 transform = mat4::translation(vec3(-15.0f, 5.0f, 0.0f)) * mat4::rotation(45.0f, vec3(0, 0, 1));
-    Group *group = new Group(mat4::translation(vec3(-15.0f, 5.0f, 0.0f)));
-    group->add(new Sprite(0, 0, 6, 3, vec4(1, 1, 1, 1)));
 
-    // mat4 t2 = mat4::translation(vec3(0.5, 0.5, 0.0f)) * mat4::rotation(-90.0f, vec3(0, 0, 1));
-    Group *button = new Group(mat4::translation(vec3(0.5f, 0.5f, 0.0f)));
-    button->add(new Sprite(0.0f, 0.0f, 5.0f, 2.0f, vec4(1, 0, 1, 1)));
-    button->add(new Sprite(0.5f, 0.5f, 3.0f, 1.0f, vec4(0.2f, 0.3f, 0.8f, 1)));
-    group->add(button);
-
-    layer.add(group);
-#endif
-
-    TileLayer layer2(&shader2);
-    layer2.add(new Sprite(-2, -2, 4, 4, vec4(1, 0, 1, 1)));
     glActiveTexture(GL_TEXTURE0);
 
     Texture texture("test.png");
@@ -83,23 +61,8 @@ int main()
     {
         window.clear();
         window.getMousePosition(x, y);
-        // shader.setUniform("light_pos", vec2((float)(x * 32.0f / 960.0f - 16.0f), (float)(9.0f - y * 18.0f / 540.0f)));
-        // shader2.setUniform("light_pos", vec2((float)(x * 32.0f / 960.0f - 16.0f), (float)(9.0f - y * 18.0f / 540.0f)));
-        // shader2.enable();
-
-        // layer.render();
-        // layer2.render();
-
-        glBegin(GL_QUADS);
-		glTexCoord2f(0, 0);
-		glVertex2f(0, 0);
-		glTexCoord2f(0, 1);
-		glVertex2f(0, 4);
-		glTexCoord2f(1, 1);
-		glVertex2f(4, 4);
-		glTexCoord2f(1, 0);
-		glVertex2f(4, 0);
-        glEnd();
+        shader.setUniform("light_pos", vec2((float)(x * 32.0f / 960.0f - 16.0f), (float)(9.0f - y * 18.0f / 540.0f)));
+        layer.render();
 
         window.update();
 
