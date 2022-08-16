a43f93b44c9fd888eb17b2a5d55b1e34e61b272f
brccabral <cabral.brc@gmail.com>
2022-08-15 22:21:14 -0700

    add transparency

--- Sparky/main.cpp
+++ Sparky/main.cpp
@@ -23,7 +23,7 @@ int main()
 	using namespace maths;
 
 	Window window("Sparky!", 960, 540);
-	//
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
 	mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
 
--- Sparky/src/graphics/layers/layer.cpp
+++ Sparky/src/graphics/layers/layer.cpp
@@ -31,7 +31,7 @@ namespace sparky::graphics
 		m_Renderer->begin();
 		for (const Renderable2D *renderable : m_Renderables)
 		{
-			renderable->submit(m_Renderer);
+			//renderable->submit(m_Renderer);
 		}
 		m_Renderer->drawString("ABab?", maths::vec3(0, 0, 0), maths::vec4(1, 0, 0, 1));
 
