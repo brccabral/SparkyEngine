cbf858653217cd31f15b7ecf492b7892ef2fdc13
brccabral <cabral.brc@gmail.com>
2022-08-17 14:51:25 -0700

    play a sample file

--- Sparky/main.cpp
+++ Sparky/main.cpp
@@ -1,3 +1,60 @@
+
+#if 1
+
+#include "gorilla/ga.h"
+#include "gorilla/gau.h"
+#include <stdio.h>
+#include <string>
+
+static void setFlagAndDestroyOnFinish(ga_Handle *in_handle, void *in_context)
+{
+	gc_int32 *flag = (gc_int32 *)(in_context);
+	*flag = 1;
+	ga_handle_destroy(in_handle);
+}
+
+int main()
+{
+	gau_Manager *mgr;
+	ga_Mixer *mixer;
+	ga_Sound *sound;
+	ga_Handle *handle;
+	gau_SampleSourceLoop *loopSrc = 0;
+	gau_SampleSourceLoop **pLoopSrc = &loopSrc;
+	gc_int32 loop = 0;
+	gc_int32 quit = 0;
+
+	/* Initialize library + manager */
+	gc_initialize(0);
+	mgr = gau_manager_create();
+	mixer = gau_manager_mixer(mgr);
+
+	/* Create and play shared sound */
+	if (!loop)
+		pLoopSrc = 0;
+	std::string filename = "Evacuate.wav";
+	sound = gau_load_sound_file(filename.c_str(), "wav");
+	handle = gau_create_handle_sound(mixer, sound, &setFlagAndDestroyOnFinish, &quit, pLoopSrc);
+	ga_handle_play(handle);
+
+	/* Bounded mix/queue/dispatch loop */
+	while (!quit)
+	{
+		gau_manager_update(mgr);
+		printf("%d / %d\n", ga_handle_tell(handle, GA_TELL_PARAM_CURRENT), ga_handle_tell(handle, GA_TELL_PARAM_TOTAL));
+		gc_thread_sleep(1);
+	}
+
+	/* Clean up sound */
+	ga_sound_release(sound);
+
+	/* Clean up library + manager */
+	gau_manager_destroy(mgr);
+	gc_shutdown();
+
+	return 0;
+}
+#else
 // #include <iostream>
 // #include <vector>
 #include <GL/glew.h>
@@ -108,3 +165,4 @@ int main()
 
 	return 0;
 }
+#endif
