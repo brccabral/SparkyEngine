939b49bc59a10df8f61f8c68c11ec346c51d478e
brccabral <cabral.brc@gmail.com>
2022-08-18 08:41:08 -0700

    remove large wav file

--- Sparky/main.cpp
+++ Sparky/main.cpp
@@ -70,7 +70,6 @@ int main()
 	shader.setUniform(
 		"pr_matrix", mat4::orthographic(-16.0f, 16.0f, -9.0f, 9.0f, -1.0f, 1.0f));
 
-	SoundManager::add(new Sound("Evacuate", "Evacuate.wav"));
 	SoundManager::add(new Sound("BomDia", "BomDia.ogg"));
 	std::string currentSound = "Evacuate";
 
