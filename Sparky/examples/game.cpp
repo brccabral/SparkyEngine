c77fc8265b9d1a35c025c6072acc202c90e5178f
brccabral <cabral.brc@gmail.com>
2022-08-18 23:58:12 -0700

    added light effect

--- Sparky/examples/game.cpp
+++ Sparky/examples/game.cpp
@@ -12,6 +12,7 @@ private:
 	Layer *layer;
 	Label *fps;
 	Sprite *sprite;
+	Shader *shader;
 public:
 	Game()
 		:window(nullptr), layer(nullptr), fps(nullptr)
@@ -28,9 +29,10 @@ public:
 	{
 		window = createWindow("Test Game", 960, 540);
 		FontManager::get()->setScale(window->getWidth() / 32.0f, window->getHeight() / 18.0f);
+		shader = new Shader("src/shaders/basic.vert", "src/shaders/basic.frag");
 		// the orthographic matrix makes the center of the window to be 0,0
 		// so, the window is x = [-16 to 16] left to right and y = [-9 to 9] bottom to top
-		layer = new Layer(new BatchRenderer2D(), new Shader("src/shaders/basic.vert", "src/shaders/basic.frag"), mat4::orthographic(-16.0f, 16.0f, -9.0f, 9.0f, -1.0f, 1.0f));
+		layer = new Layer(new BatchRenderer2D(), shader, mat4::orthographic(-16.0f, 16.0f, -9.0f, 9.0f, -1.0f, 1.0f));
 		layer->add(new Sprite(0.0f, 0.0f, 4, 4, 0xffffffff));
 		sprite = new Sprite(6.0f, 3.0f, 4, 4, new Texture("test.png"));
 		layer->add(sprite);
@@ -61,6 +63,11 @@ public:
 			sprite->position.x += speed;
 		if (window->isKeyPressed(GLFW_KEY_LEFT))
 			sprite->position.x -= speed;
+		
+		double x, y;
+		window->getMousePosition(x, y);
+		shader->setUniform("light_pos", vec2((float)(x * 32.0f / window->getWidth() - 16.0f),
+			(float)(9.0f - y * 18.0f / window->getHeight())));
 	}
 
 };
