e3f1db07b993923b378841e6100415a19c63d0cd
brccabral <cabral.brc@gmail.com>
2022-08-21 19:37:34 -0700

    exclude modules from freeimage

--- Dependencies/FreeImage/Source/FreeImage/Plugin.cpp
+++ Dependencies/FreeImage/Source/FreeImage/Plugin.cpp
@@ -265,15 +265,15 @@ FreeImage_Initialise(BOOL load_local_plugins_only) {
 	        s_plugins->AddNode(InitHDR);
 			s_plugins->AddNode(InitG3);
 			s_plugins->AddNode(InitSGI);
-			s_plugins->AddNode(InitEXR);
+			//s_plugins->AddNode(InitEXR); // emcc generates unnecessary big file
 			s_plugins->AddNode(InitJ2K);
 			s_plugins->AddNode(InitJP2);
 			s_plugins->AddNode(InitPFM);
 			s_plugins->AddNode(InitPICT);
-			s_plugins->AddNode(InitRAW);
+			//s_plugins->AddNode(InitRAW); // emcc fails to compile this module
 			s_plugins->AddNode(InitWEBP);
 #if !(defined(_MSC_VER) && (_MSC_VER <= 1310))
-			s_plugins->AddNode(InitJXR);
+			//s_plugins->AddNode(InitJXR); // emcc fails to compile this module
 #endif // unsupported by MS Visual Studio 2003 !!!
 			
 			// external plugin initialization
