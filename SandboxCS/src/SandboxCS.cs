c943cf8339e93d2ded922ce60280437edf4918f1
brccabral <cabral.brc@gmail.com>
2022-09-12 12:19:16 -0700

    added c# sandbox

---
+++ SandboxCS/src/SandboxCS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SparkyCLI;
+
+public class Sandbox
+{
+    public static void Main(string[] args)
+    {
+        Window window = new Window("Test", 960, 540);
+        while (!window.Closed())
+        {
+            Vector2 mouse = window.GetMousePosition();
+            Console.WriteLine("{0}, {1}", mouse.x, mouse.y);
+            window.Update();
+        }
+    }
+}
