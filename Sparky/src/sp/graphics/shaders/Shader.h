16ea3231f919b8fbca5a55cca1615d1790d74a3a
brccabral <cabral.brc@gmail.com>
2022-09-19 22:15:01 -0700

    improved Shader pipeline

--- Sparky/src/sp/graphics/shaders/Shader.h
+++ Sparky/src/sp/graphics/shaders/Shader.h
@@ -5,6 +5,7 @@
 #include <sp/utils/FileUtils.h>
 #include <sp/utils/StringUtils.h>
 #include <sp/maths/maths.h>
+#include "ShaderUniform.h"
 
 namespace sp
 {
@@ -24,14 +25,13 @@ namespace sp
 			{
 				UNKNOWN, VERTEX, FRAGMENT
 			};
-			struct ShaderSource
-			{
-
-			};
 
 			String m_Name, m_Path;
 			String m_Source;
+			String m_VertexSource, m_FragmentSource;
 			uint m_ShaderID; // shader ID given by openGl
+
+			std::vector<ShaderUniformDeclaration *> m_Uniforms;
 		public:
 			Shader(const String &name, const String &source);
 			~Shader();
@@ -49,12 +49,28 @@ namespace sp
 			void SetUniform(const String &name, const maths::vec3 &vector);
 			void SetUniform(const String &name, const maths::vec4 &vector);
 
+			void SetUniform(uint location, const maths::mat4 &matrix);
+			void SetUniform(uint location, int value);
+			void SetUniform(uint location, int *value, int count);
+			void SetUniform(uint location, float value);
+			void SetUniform(uint location, float *value, int count);
+			void SetUniform(uint location, const maths::vec2 &vector);
+			void SetUniform(uint location, const maths::vec3 &vector);
+			void SetUniform(uint location, const maths::vec4 &vector);
+
+			void ResolveAndSetUniforms(byte *data, uint size);
+
+			inline const std::vector<ShaderUniformDeclaration *> &GetUniformDeclarations() const { return m_Uniforms; }
 		private:
 			uint Load(const String &vertSource, const String &fragSource);
 
 			int GetUniformLocation(const String &name);
 
-			void PreProcess(const String &source, String *shaders);
+			void PreProcess(const String &source, String **shaders);
+			void ParseUniforms(const std::vector<String> &lines);
+			ShaderUniformDeclaration::Type GetUniformTypeFromString(const String &token);
+			void ResolveUniforms();
+			void ResolveAndSetUniform(ShaderUniformDeclaration *uniform, byte *data);
 		public:
 			static Shader *FromFile(const String &name, const String &filepath);
 			static Shader *FromSource(const String &name, const String &source);
