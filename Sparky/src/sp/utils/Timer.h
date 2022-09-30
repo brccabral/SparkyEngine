cd661d2322c006510563ee87ddf6e261fc2fb7ab
brccabral <cabral.brc@gmail.com>
2022-09-30 11:58:32 -0700

    moved Timer to platform specific

--- Sparky/src/sp/utils/Timer.h
+++ Sparky/src/sp/utils/Timer.h
@@ -2,59 +2,18 @@
 
 #include "sp/Common.h"
 
-#define STD_CHRONO 1
-
-#if STD_CHRONO
-
-#include <chrono>
-
 namespace sp
 {
+
 	class SP_API Timer
 	{
-	private:
-		typedef std::chrono::high_resolution_clock HighResolutionClock;
-		typedef std::chrono::duration<float, std::milli> milliseconds_type;
-		std::chrono::time_point<HighResolutionClock> m_Start;
 	public:
+		// Creates and starts timer
 		Timer();
-
+		// Resets and restarts timer
 		void Reset();
-
+		// Returns time in milliseconds
 		float Elapsed();
-
 	};
+
 }
-
-#else
-
-// include OS specific timing library
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-// Windows
-#include <Windows.h>
-#else
-// Linux
-#include <time.h>
-#include <stdint.h>
-#endif
-
-namespace sp
-{
-	class Timer
-	{
-	private:
-	#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-		LARGE_INTEGER m_Start;
-	#else
-		struct timespec m_Start;
-	#endif
-		double m_Frequency;
-
-	public:
-		Timer();
-		void Reset();
-		float Elapsed();
-	};
-} // namespace sp
-
-#endif
